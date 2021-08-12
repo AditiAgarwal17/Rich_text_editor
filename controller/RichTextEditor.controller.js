e531149d7c53e368ef8104646162bc2ece84d669
AditiAgarwal17 <aditiagarwal263@gmail.com>
2021-08-12 13:03:12 +0530

    Updating the content in DynamoDB

--- controller/RichTextEditor.controller.js
+++ controller/RichTextEditor.controller.js
@@ -6,10 +6,7 @@ function (Controller,MessageToast,MessageBox,aws,read){
 		onInit: function() {
 			this.initRichTextEditor(false);
 		},
-		onSave: function () {
-
-            MessageBox.success("Article Saved Successfully");
-        },
+		
 		onCancel: function () {
 			MessageBox.warning("Cancel the changes made to the Article?", {
 				actions: [MessageBox.Action.YES, MessageBox.Action.NO],
@@ -17,30 +14,67 @@ function (Controller,MessageToast,MessageBox,aws,read){
 				
 			});
 		},
-		
 		initRichTextEditor: function (bIsTinyMCE5) {
 			var that = this,
-				sHtmlValue = fetchOneByKey();
+				sHtmlValue = "";
+
+			fetchOneByKey().then(function (data) {
+				that.oRichTextEditor.mProperties.value=data.Content
+			});
+			
 				sap.ui.require(["sap/ui/richtexteditor/RichTextEditor"],
 				
 				function (RTE) {
 					that.oRichTextEditor = new RTE("myRTE", {
-						
+
 						width: "100%",
 						height: "600px",
 						customToolbar: true,
 						showGroupFont: true,
 						showGroupLink: true,
 						showGroupInsert: true,
-						value: sHtmlValue,
+						
 						ready: function () {
 							this.addButtonGroup("styleselect").addButtonGroup("table");
 						}
 					});
 
 					that.getView().byId("idVerticalLayout").addContent(that.oRichTextEditor);
+					
 			});
-		}
+			
+		},
+		onSave: function () {
+			var that=this,
+				content=that.oRichTextEditor.mProperties.value
+			let modify = function () {
+
+    
+				var params = {
+					TableName: "Articles",
+					Key: { "UUID": "2" },
+					UpdateExpression: "set updated_by = :byUser, is_deleted = :boolValue, Content= :String",
+					ExpressionAttributeValues: {
+						":byUser": "updateUser",
+						":boolValue": true,
+						":String":content
+					},
+					ReturnValues: "UPDATED_NEW"
+			
+				};
+				docClient.update(params, function (err, data) {
+			
+					if (err) {
+						console.log("users::update::error - " + JSON.stringify(err, null, 2));
+					} else {
+						console.log("users::update::success "+JSON.stringify(data) );
+					}
+				});
+			}
+			modify();
+            MessageBox.success("Article Saved Successfully");
+			
+        },
 
 	});
 });
